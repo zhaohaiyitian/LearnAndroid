df12e397d0af9d446f3f580464992a4068739a9a
wangjie <15601775572@163.com>
2024-03-13 12:30:51 +0800

    添加gradle插件

---
+++ plugin/src/main/java/com/jack/plugin/MyClass.kt
@@ -0,0 +1,4 @@
+package com.jack.plugin
+
+class MyClass {
+}
---
+++ plugin/src/main/java/com/jack/plugin/MyCustomPlugin.kt
@@ -0,0 +1,13 @@
+package com.jack.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import com.android.build.gradle.AppExtension
+
+
+class MyCustomPlugin:Plugin<Project> {
+    override fun apply(p: Project) {
+        val extension = p.extensions.getByType(AppExtension::class.java)
+        println("plugin开发")
+    }
+}
