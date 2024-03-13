d2d683bbdb1579eca2ee8d82e05bfbbd688e090d
wangjie <15601775572@163.com>
2024-03-13 12:20:06 +0800

    编译报错

--- app/src/main/java/com/jack/learn/MainActivity.kt
+++ app/src/main/java/com/jack/learn/MainActivity.kt
@@ -1,16 +1,7 @@
 package com.jack.learn
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jack.learn.ui.theme.LearnAndroidTheme
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
