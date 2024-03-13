7a4cebce65640806234b017207a560d302d60b8b
wangjie <15601775572@163.com>
2024-03-13 11:56:16 +0800

    Initial commit

---
+++ app/src/main/java/com/jack/learn/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.jack.learn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jack.learn.ui.theme.LearnAndroidTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
