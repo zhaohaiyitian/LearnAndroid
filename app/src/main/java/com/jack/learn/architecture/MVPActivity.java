98a58062eea241e3b0749643ef1c6919d7d8c293
wangjie <15601775572@163.com>
2024-03-27 19:46:56 +0800

    加入mvp

---
+++ app/src/main/java/com/jack/learn/architecture/MVPActivity.java
@@ -0,0 +1,20 @@
+package com.jack.learn.architecture;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.jack.learn.R;
+
+/**
+ * MVP核心思想：把原来(MVC)的UI逻辑抽象成View接口，把原来(MVC)的业务逻辑抽象成Presenter接口，model还是原来(MVC)的model
+ */
+public class MVPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvp);
+    }
+}
