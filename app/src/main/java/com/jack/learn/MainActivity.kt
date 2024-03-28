4f78d83d8fa0059c93e791d67a0895c5cebe49cc
wangjie <15601775572@163.com>
2024-03-28 15:21:49 +0800

    添加protobuf

--- app/src/main/java/com/jack/learn/MainActivity.kt
+++ app/src/main/java/com/jack/learn/MainActivity.kt
@@ -2,16 +2,14 @@ package com.jack.learn
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.jack.learn.aidl.BinderActivity
 import com.jack.learn.apm.APMActivity
 import com.jack.learn.designpattern.DesignPatternActivity
 import com.jack.learn.jetpack.MVVMActivity
-import com.jack.learn.jni.IAntiDebugCallback
 import com.jack.learn.jni.JNIActivity
 import com.jack.learn.kotlin.KotlinActivity
+import com.jack.learn.thirdlib.ThirdLibActivity
 import com.jack.learn.view.CustomViewActivity
 import com.jack.learn.viewpager.ViewPagerActivity
 
@@ -41,6 +39,9 @@ class MainActivity : AppCompatActivity() {
         }
         findViewById<Button>(R.id.viewPager).setOnClickListener {
             startActivity(Intent(this,ViewPagerActivity::class.java))
+        }
+        findViewById<Button>(R.id.thirdLib).setOnClickListener {
+            startActivity(Intent(this, ThirdLibActivity::class.java))
         }
     }
 
