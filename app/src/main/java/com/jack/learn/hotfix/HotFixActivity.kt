23d7c7478d5042215ae3f828b51dd2aeeb097d08
wangjie <15601775572@163.com>
2024-04-18 09:27:43 +0800

    热修复理论

---
+++ app/src/main/java/com/jack/learn/hotfix/HotFixActivity.kt
@@ -0,0 +1,12 @@
+package com.jack.learn.hotfix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jack.learn.R
+
+class HotFixActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hot_fix)
+    }
+}
