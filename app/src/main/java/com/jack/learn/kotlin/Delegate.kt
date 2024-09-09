b3def65c7dcf54b1f9a99ee091ade2a0dbd2e5c4
wangjie <1541777835@qq.com>
2024-09-09 22:41:12 +0800

    kotlin by lazy

--- app/src/main/java/com/jack/learn/kotlin/Delegate.kt
+++ app/src/main/java/com/jack/learn/kotlin/Delegate.kt
@@ -2,31 +2,25 @@ package com.jack.learn.kotlin
 
 import kotlin.reflect.KProperty
 
+/**
+ * 定义一个用于属性委托的对象
+ */
 class Delegate {
 
     operator fun getValue(thisRef: Any?,property: KProperty<*>): String {
-        return "$thisRef,${property.name}"
+        return "getValue： 所属对象：$thisRef, 参数名称：${property.name}"
     }
 
-
-}
-
-
-interface IUserAction {
-    fun attack()
-
-    val filed: Int
-        get() = 0
-}
-class UserDelegate(private val action: IUserAction): IUserAction by action
-
-class UserActionImpl: IUserAction {
-    override fun attack() {
-
+    operator fun setValue(thisRef: Any?,property: KProperty<*>,value: String) {
+        println("setValue: 所属对象：$thisRef, 参数名称：${property.name}, 设置的值：$value")
     }
+
 }
+
 fun main() {
-    UserDelegate(UserActionImpl()).apply {
-        attack()
-    }
+    var demoString by Delegate()
+    // 使用demoString，这时候会去调用Delegate的getValue方法
+    println(demoString)
+    // 设置demoString的值，这时候去会调用setValue方法
+    demoString = "main"
 }
