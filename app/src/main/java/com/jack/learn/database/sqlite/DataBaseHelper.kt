a38df5cd676f8f6bc8e98cf65ee2beb1c32e88ac
wangjie <1541777835@qq.com>
2024-12-12 15:53:47 +0800

    数据库插入优化

--- app/src/main/java/com/jack/learn/database/sqlite/DataBaseHelper.kt
+++ app/src/main/java/com/jack/learn/database/sqlite/DataBaseHelper.kt
@@ -18,12 +18,32 @@ class DataBaseHelper(
 ) : SQLiteOpenHelper(context, name, factory, version) {
 
 
-    // 第一次创建数据库的时候被调用
-    override fun onCreate(db: SQLiteDatabase?) {
+    private val DATABASE_NAME = "my_database.db"
+    private val DATABASE_VERSION = 1
+    private val TABLE_NAME = "users"
+    private val COLUMN_ID = "id"
+    private val COLUMN_NAME = "name"
+    private val COLUMN_AGE = "age"
 
+    // 第一次创建数据库的时候被调用
+    override fun onCreate(db: SQLiteDatabase) {
+        val createTableQuery = "CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_AGE + " INTEGER)"
+        db.execSQL(createTableQuery)
     }
 
     // 升级数据库的回调
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    private fun insertUser(db: SQLiteDatabase,name: String,age: Long) {
+        val insertQuery = "INSERT INTO $TABLE_NAME($COLUMN_NAME, $COLUMN_AGE) VALUES (?,?)"
+        val stmt = db.compileStatement(insertQuery)
+        stmt.bindString(1,name)
+        stmt.bindLong(2,age)
+        // 执行预编译语句
+        stmt.executeInsert()
     }
 }
